70e01af9df0f58b05cd71a54c3cd11a3a282273f
jaceeo <juneeye92@gmail.com>
2022-10-30 16:21:10 +0900

    Naver Api

--- nepp-movie/src/components/practice/api/Post.jsx
+++ nepp-movie/src/components/practice/api/Post.jsx
@@ -3,18 +3,22 @@ import axios from "axios";
 import useAsync from "./useAsync";
 
 function Post() {
-  const [title, setTitle] = useState("");
+  const [title, setTitle] = useState("23");
   const [state, fetchData] = useAsync(async () => {
-    return axios.get("http://localhost:8000/posts");
+    return axios.get("v1/search/book.json", {
+      params: {
+        query: title,
+        display: 100,
+      },
+      headers: {
+        "X-Naver-Client-Id": process.env.REACT_APP_ClientId,
+        "X-Naver-Client-Secret": process.env.REACT_APP_ClientSecret,
+      },
+    });
   });
 
   const onSubmit = async () => {
-    let result = await axios.post("http://localhost:8000/posts", {
-      title,
-      author: "jace",
-    });
-    console.log(result);
-    fetchData();
+    let result = await fetchData();
   };
 
   const onDelete = async (id) => {
@@ -27,6 +31,7 @@ function Post() {
 
   if (loading) return <div>로딩 중...</div>;
   if (error) return <div>에러 발생!</div>;
+  if (!data) return;
 
   return (
     <div>
@@ -37,12 +42,19 @@ function Post() {
         onChange={({ target: { value } }) => setTitle(value)}
       />
       <button onClick={onSubmit}>Submit</button>
-      {data?.map((post) => (
+
+      {data.items.map((item) => (
+        <li key={item.isbn}>
+          {item.title}({item.author})
+          <img src={item.image} alt="" />
+        </li>
+      ))}
+      {/* {data?.map((post) => (
         <li key={post.id}>
           {post.title}
           <button onClick={() => onDelete(post.id)}>Delete</button>
         </li>
-      ))}
+      ))} */}
     </div>
   );
 }
